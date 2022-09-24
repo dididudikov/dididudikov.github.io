f46668f75b4551967ff8c37a7f730d75772ce4f4
dididudikov <38637149+dididudikov@users.noreply.github.com>
2022-09-24 15:40:45 +0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d1ce1d7540f3d65008491f204f52f43e",
-"/": "d1ce1d7540f3d65008491f204f52f43e",
-"main.dart.js": "db8c911a728258606b93c145606943a1",
+"index.html": "23885f810b4e4f37eb8d0b4aac400928",
+"/": "23885f810b4e4f37eb8d0b4aac400928",
+"main.dart.js": "5c172e5dc3c919e832995d77019c5e00",
 "manifest.json": "7faacbaaecdf56605a3af79a88f13e70",
 "version.json": "cb292393e2df9458916bbfd2c97f5263"
 };
